5d59c5fc6ae30907d60c9a373b22e7fd03b8a8e6
Andy Karolin <andykarolin@Andys-MacBook-Pro.local>
2015-05-15 12:39:42 -0400

    Initial Commit

---
+++ SupermarketPricing/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SupermarketPricing
+//
+//  Created by Andy Karolin on 5/15/15.
+//  Copyright (c) 2015 Andy Karolin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SupermarketPricing/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SupermarketPricing
+//
+//  Created by Andy Karolin on 5/15/15.
+//  Copyright (c) 2015 Andy Karolin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
